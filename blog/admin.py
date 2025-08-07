0746df752a77c6536ce55f9f8fa2db8f717ed646
Abduolimov03 <asadbekabduolimov33@gmail.com>
2025-08-07 12:54:59 +0500

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Comment, Tag, Post, Rating
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {"slug":('name',)}
+
+admin.site.register(Comment)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {"slug":('name',)}
+
+admin.site.register(Post)
+admin.site.register(Rating)
