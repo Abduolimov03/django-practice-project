854c0ce4c73c701f2f8efa8fc292b6841eee13f4
Abduolimov03 <asadbekabduolimov33@gmail.com>
2025-08-08 21:07:45 +0500

    Add Rating model and update Post relationships

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
+from django.db.models import Avg
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(verbose_name='Category name', max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
@@ -9,7 +9,7 @@ class Category(models.Model):
         return str(self.name)
 
 class Tag(models.Model):
-    name = models.CharField(verbose_name='Category name', max_length=250)
+    name = models.CharField(verbose_name='Tag name', max_length=250)  # verbose_name o'zgartirildi
     slug = models.SlugField(max_length=250, unique=True)
 
     def __str__(self):
@@ -17,10 +17,10 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=550, verbose_name="Post title")
-    body = models.TextField(verbose_name="Post title")
+    body = models.TextField(verbose_name="Post body")  # verbose_name to'g'irlandi
     author = models.CharField(verbose_name="Post author", default='Admin', max_length=100)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
-    tag = models.ManyToManyField(Tag )
+    tag = models.ManyToManyField(Tag)
     views = models.PositiveSmallIntegerField(default=0)
     publish_date = models.DateTimeField(verbose_name="Published time", auto_now_add=True)
     published = models.BooleanField(default=True)
@@ -29,8 +29,14 @@ class Post(models.Model):
     def __str__(self):
         return str(self.title)
 
+    def get_avg_rating(self):
+        avg = self.ratings.aggregate(Avg('value'))['value__avg']
+        if avg is None:
+            return "No ratings"
+        return round(avg, 1)
+
 class Comment(models.Model):
-    author = models.CharField(verbose_name="Comment  author",blank=False , max_length=100)
+    author = models.CharField(verbose_name="Comment author", blank=False, max_length=100)
     comment = models.TextField(verbose_name='Comment')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
@@ -39,12 +45,7 @@ class Comment(models.Model):
 
 class Rating(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings')
-    value = models.PositiveSmallIntegerField(verbose_name='Post rating', default=0)
+    value = models.PositiveIntegerField(verbose_name='Post rating', default=0)
 
     def __str__(self):
         return str(self.value)
-
-
-
-
-
